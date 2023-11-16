337a1cd6f072a672e19800a74daafbe1c738b427
Andrea Samori <andrea.samori3@studio.unibo.it>
2023-11-16 18:58:54 +0100

    starting testing DeathNote, implemented testGetRoule()

--- 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 83-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,33 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.api.UseDeathNote;
+
 class TestDeathNote {
+    final int WRONG_NUM_RULE = 0;
+    DeathNote DN = new UseDeathNote();
+
+
+    /**
+     * Cheks if wrong rule number is given, we get an IllegalArgumentExeption back.
+    */
+    @Test
+    public void testGetRule(){
+        try{
+            DN.getRule(WRONG_NUM_RULE);
+            Assertions.fail("Is expected that the getRoule() fails, but it does not");
+        }catch (IllegalArgumentException e){
+            assertEquals("""
+                Given rule number is smaller than 1 or larger
+                than the number of rules
+                """, e);
+        }
+    }
 
 }
